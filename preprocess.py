f6c4b9a37aaa571823651c6bc825655aee8ea87b
Basar-Bashir <basarbashir2019@gmail.com>
2026-06-03 22:53:19 +0530

    add lfs files

--- preprocess.py
+++ preprocess.py
@@ -1,27 +1,3 @@
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-
-data = pd.read_csv("fer2013.csv")
-
-pixels = data['pixels'].tolist()
-faces = []
-
-for pixel in pixels:
-    face = np.array(pixel.split(), dtype='float32')
-    face = face.reshape(48, 48, 1)
-    faces.append(face)
-
-faces = np.array(faces) / 255.0
-labels = pd.get_dummies(data['emotion']).values
-
-X_train, X_test, y_train, y_test = train_test_split(
-    faces, labels, test_size=0.2, random_state=42
-)
-
-np.save("dataset/X_train.npy", X_train)
-np.save("dataset/X_test.npy", X_test)
-np.save("dataset/y_train.npy", y_train)
-np.save("dataset/y_test.npy", y_test)
-
-print("Preprocessing complete.")
+version https://git-lfs.github.com/spec/v1
+oid sha256:c79e5f045b2eeb6b810ca0913f092afba0b1fcfa18fbeacc18752b9781cb7353
+size 709
